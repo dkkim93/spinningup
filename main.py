fac9f196a473eff897ec8b992cddd3a78c6994db
Dong-Ki Kim <dkkim93@mit.edu>
2020-10-27 11:08:00 -0400

    Added pendulum support

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import gym_env
 from spinup import ppo_pytorch as ppo
 from gym_env.wrapper import PendulumCostWrapper
 
+env = gym.make('Pendulum-v0')
+env._max_episode_steps = 100
+env = PendulumCostWrapper(env)
+
 ppo(
-    env_fn=lambda: PendulumCostWrapper(gym.make('Pendulum-v0')), 
+    env_fn=lambda: env, 
     ac_kwargs=dict(hidden_sizes=[16] * 2),
     gamma=0.99,
     max_ep_len=1000,
